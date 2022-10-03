340a58a96db753b496d046d14eece5dcfc7fc3c6
owenpelito <owen@nabepero.co.jp>
2022-10-03 21:30:17 +0800

    usermanagement/soft-deletejs

--- resources/views/admin/admin_master.blade.php
+++ resources/views/admin/admin_master.blade.php
@@ -7,9 +7,9 @@
     <meta name="description" content="">
     <meta name="author" content="">
     <link rel="icon" href="{{ asset('backend/images/favicon.ico') }}">
-
-    <title>School Management System - Dashboard</title>
     
+    <title>School Management System - Dashboard</title>
+
 	<!-- Vendors Style--> 
 	<link rel="stylesheet" href="{{ asset('backend/css/vendors_css.css') }}">
 	  
@@ -18,6 +18,8 @@
 	<link rel="stylesheet" href="{{ asset('backend/css/skin_color.css') }}">
      
 	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css" >
+
+  
   </head>
 
 <body class="hold-transition dark-skin sidebar-mini theme-primary fixed">
@@ -40,7 +42,7 @@
   
 </div>
 <!-- ./wrapper -->
-  	
+
 	
 	<!-- Vendor JS -->
 	<script src="{{ asset('backend/js/vendors.min.js') }}"></script>
@@ -50,12 +52,50 @@
 	<script src="{{ asset('../assets/vendor_components/apexcharts-bundle/dist/apexcharts.js') }}"></script>
 	<script src="{{ asset('../assets/vendor_components/datatable/datatables.min.js') }}"></script>
 	<script src="{{ asset('backend/js/pages/data-table.js') }}"></script>
+  <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+  <script type="text/javascript">
+    $(function(){
+      $(document).on('click','#delete',function(e){
+          e.preventDefault();
+          var link = $(this).attr("href");
+  
+    
+                    Swal.fire({
+                      title: 'Are you sure?',
+                      text: "Delete This Data?",
+                      icon: 'warning',
+                      showCancelButton: true,
+                      confirmButtonColor: '#3085d6',
+                      cancelButtonColor: '#d33',
+                      confirmButtonText: 'Yes, delete it!'
+                    }).then((result) => {
+                      if (result.isConfirmed) {
+                        window.location.href = link
+                        Swal.fire(
+                          'Deleted!',
+                          'Your file has been deleted.',
+                          'success'
+                        )
+                      }
+                    }) 
+  
+  
+      });
+  
+    });
+  
+  
+  </script> 
+
 
 	<!-- Sunny Admin App -->
 	<script src="	 {{ asset('backend/js/template.js') }}"></script>
 	<script src="	 {{ asset('backend/js/pages/dashboard.js') }}"></script>
 	
+
 	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+
 
 <script>
  @if(Session::has('message'))
